71c20ca5137d2527fb661a5da7cc175384d77945
codematrix <ross_pellegrino@hotmail.com>
2015-12-08 22:39:17 -0500

    Adding ExceptionInterceptManagerTest project.

---
+++ test/AspNet.Plus.Infrastructure.ExceptionIntercept.Tests/ExceptionInterceptManagerTest.cs
@@ -0,0 +1,25 @@
+﻿using AspNet.Plus.Infrastructure.ExceptionInterceptHandler.Interfaces;
+using Microsoft.AspNet.Diagnostics;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspNet.Plus.Infrastructure.ExceptionIntercept.Tests
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class Class1
+    {
+        [Fact]
+        public void UniTest1()
+        {
+           // var xxx = new Mock<IExceptionInterceptContext>();
+            // new Mock<IExceptionHandlerFeature>();
+            
+
+        }
+    }
+}
