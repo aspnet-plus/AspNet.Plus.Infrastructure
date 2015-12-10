b8a25d3bebcd810a1e0950eb5aa78d398549c947
codematrix <ross_pellegrino@hotmail.com>
2015-12-10 09:53:53 -0500

    Updating dnx version to rc1-final

--- test/AspNet.Plus.Infrastructure.ExceptionIntercept.Tests/ExceptionInterceptManagerTest.cs
+++ test/AspNet.Plus.Infrastructure.ExceptionIntercept.Tests/ExceptionInterceptManagerTest.cs
@@ -18,32 +18,32 @@ namespace AspNet.Plus.Infrastructure.ExceptionIntercept.Tests
         //private IMock<HttpContext> PrepareHttpContextMock()
         //{
         //    var httpContext = new Mock<HttpContext>();
-            
+
         //    httpContext.Setup(x => x.Features).Returns(new Mock<IFeatureCollection>().Object);
-            
+
         //    return httpContext;
         //}
 
-        //[Fact]
-        //public void InterceptExceptionAsync_AddingIntercetHandlers_HandlersWereInvokedInSequence()
-        //{
-        //    // arrange            
-        //    var unit = new ExceptionInterceptManager();
-        //    var handler1 = new Mock<IExceptionInterceptHandler>();
-        //    var handler2 = new Mock<IExceptionInterceptHandler>();
-        //    var handler3 = new Mock<IExceptionInterceptHandler>();
-        //    var httpContext = new Mock<HttpContext>();
-            
-
-
-        //    //var xx = httpContext.Object.Features;
+        [Fact]
+        public void InterceptExceptionAsync_AddingIntercetHandlers_HandlersWereInvokedInSequence()
+        {
+            //    // arrange            
+            //    var unit = new ExceptionInterceptManager();
+            //    var handler1 = new Mock<IExceptionInterceptHandler>();
+            //    var handler2 = new Mock<IExceptionInterceptHandler>();
+            //    var handler3 = new Mock<IExceptionInterceptHandler>();
+            //    var httpContext = new Mock<HttpContext>();
 
 
 
-        //    // var xxx = new Mock<IExceptionInterceptContext>();
-        //    // new Mock<IExceptionHandlerFeature>();
+            //    //var xx = httpContext.Object.Features;
 
 
-        //}
+
+            //    // var xxx = new Mock<IExceptionInterceptContext>();
+            //    // new Mock<IExceptionHandlerFeature>();
+
+
+        }
     }
 }
