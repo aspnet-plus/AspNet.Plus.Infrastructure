a2062ac028d5865947c5c7debad8aae04bf872c7
codematrix <ross_pellegrino@hotmail.com>
2019-02-08 10:38:25 -0500

    Adding .net core 2.2 support, changed nuget version

--- samples/ExceptionInterceptHandler/Mvc.ExceptionInterceptSample/Program.cs
+++ samples/ExceptionInterceptHandler/Mvc.ExceptionInterceptSample/Program.cs
@@ -1,5 +1,5 @@
 ﻿// ASP.NET.Plus
-// Copyright (c) 2016 AspNet.Plus.
+// Copyright (c) 2019 AspNet.Plus.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
 
 using Microsoft.AspNetCore.Builder;
